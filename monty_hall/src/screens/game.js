bbc5554c0f583d4409b6cbcec2069443dc8258ae
danielsanchezsa <danielsanchez.2000@hotmail.com>
2022-04-19 12:02:29 -0500

    init

---
+++ monty_hall/src/screens/game.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import "../css/game.css"
+
+
+export default function Game() {
+
+    return(
+        <div className='background'>
+        Game
+        </div>
+    )
+}
+
+
+
